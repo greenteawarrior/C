2a787f5a2432b277a7cf8162d1201c0b2b27c890
Emily Wang <greenteawarrior@pangolin.(none)>
2013-11-22 23:51:31 -0500

    hello world!

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+    //The first C program! 
+    //Also playing with escaping hard-to-type or invisible characters.
+    printf("hello, world\n");
+    printf("\t hello, world and then ");
+    printf("they say \"hello world!\"");
+    printf("\n \b hi!");
+    printf("\\");
+    printf("\n");
+}
