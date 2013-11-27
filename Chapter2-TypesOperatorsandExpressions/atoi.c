38430fff465db81967c672811df77aed60ac3ce6
Emily Wang <emily.wang@students.olin.edu>
2013-11-26 19:04:01 -0500

    notes on variables/constants/declarations and first attempts with type conversion

---
+++ Chapter2-TypesOperatorsandExpressions/atoi.c
@@ -0,0 +1,30 @@
+/*
+C Programming Language 2.7:
+Type Conversions
+11/25/2013, Emily Wang
+*/
+
+//takes in a string of numbers and prints out the integer equivalent
+
+#include <stdio.h>
+
+//declare it!
+char numberstr[] = "123456"; //numberstr of interest
+int atoi(char numberstr[]); //our helper function
+
+main()
+{
+    //run the atoi function and print the result
+    printf("%i \n", atoi(numberstr));
+}
+
+// atoi : convert numberstr to integer
+int atoi(char numberstr[])
+{
+    int i, n;
+
+    n = 0;
+    for (i = 0; numberstr[i] >= '0' && numberstr[i] <= '9'; ++i)
+        n = 10 * n + (numberstr[i] - '0');
+    return n;
+}
